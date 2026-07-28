4d6c5f3e00d627b47a7811903d37dc6c94425926
Arthur <arthurdev.contatoo@gmail.com>
2026-07-28 11:46:45 -0300

    corrige formatacao do token obtido

--- src/main/java/agendador/agendadortarefas/business/TarefaService.java
+++ src/main/java/agendador/agendadortarefas/business/TarefaService.java
@@ -21,8 +21,20 @@ public class TarefaService {
     public TarefaDTO adicionarTarefa(TarefaDTO tarefaDTO, String token){
         tarefaDTO.setDataCriacao(LocalDateTime.now());
         tarefaDTO.setStatus(StatusNotificacaoEnum.PENDENTE);
-        tarefaDTO.setEmailUsuario(jwtUtil.extractUsername(token)); // Setando o e-mail do usuário autenticado
+
+        // Setando o e-mail do usuário autenticado
+        tarefaDTO.setEmailUsuario(jwtUtil.extractUsername(getTokenFormatado(token)));
         Tarefa tarefaAdicionar = tarefaMapper.toTarefa(tarefaDTO);
         return tarefaMapper.toTarefaDTO(tarefaRepository.save(tarefaAdicionar));
+    }
+
+    // Retira a inicial e espaçamento do token
+    public String getTokenFormatado(String token){
+        String prefixoToken = "Bearer ";
+        if(token.startsWith(prefixoToken)){
+            token = token.substring(prefixoToken.length());
+        }
+
+        return token;
     }
 }
